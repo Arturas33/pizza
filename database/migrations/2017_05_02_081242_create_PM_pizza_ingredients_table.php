6ad6139f90b8cdd29e1953490c5c0a617426cdb5
Arturas33 <jasinskis330@gmail.com>
2017-05-02 11:56:58 +0300

    added migrations

---
+++ database/migrations/2017_05_02_081242_create_PM_pizza_ingredients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePMPizzaIngredientsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('PM_pizza_ingredients', function(Blueprint $table)
+		{
+			$table->integer('count')->unique('count');
+			$table->string('id', 36)->unique('id');
+			$table->timestamps();
+			$table->softDeletes();
+			$table->string('name');
+			$table->text('description', 65535)->nullable();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('PM_pizza_ingredients');
+	}
+
+}
